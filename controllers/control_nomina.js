bc679797b11c67e5a9d477f9341652c1de6267de
DilanPlusV2 <xxxyosoydilanxx@gmail.com>
2023-12-19 15:00:08 -0500

    Error body parser 6

--- controllers/control_nomina.js
+++ controllers/control_nomina.js
@@ -4,7 +4,7 @@ const { where } = require('sequelize');
 const models = require('../models');
 const { Op } = require('sequelize');
 
-// Configura Moment.js para la zona horaria de Colombia
+// Configura Moment.js para la zona horaria de Locombia
 moment.tz.setDefault('America/Bogota');
 
 // Función para calcular la nómina del empleado
