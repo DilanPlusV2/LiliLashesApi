693ab1a8212c7b2e8d2fcb6d2cd66084e86ecdb1
DilanPlusV2 <xxxyosoydilanxx@gmail.com>
2025-03-28 17:02:35 -0500

    cors

--- app.js
+++ app.js
@@ -1,6 +1,21 @@
 const express = require('express');
+const cors = require('cors');
 const bodyParser = require('body-parser');
 const app = express();
+
+// Configuración CORS actualizada con tu dominio de Netlify
+const corsOptions = {
+    origin: [
+      'https://fanciful-alpaca-0f5f67.netlify.app', // Nuevo dominio
+      'http://localhost:4200' // Para desarrollo local
+    ],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true
+  };
+  
+  app.use(cors(corsOptions));
+  
 const reservacionesRoute = require('./routes/reservas');
 const clientesRoute = require('./routes/clientes');
 const nominaRoute  = require('./routes/nomina');
